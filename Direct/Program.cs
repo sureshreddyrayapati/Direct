74badcea85a45eac3a8428f5279bac03d59ca6f8
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-19 19:14:29 +0530

    Add project files.

---
+++ Direct/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Direct;
+Tables tables = new Tables();
+Console.WriteLine("Enter a number Table");
+int a=int.Parse(Console.ReadLine());
+tables.Multiples(10);
---
+++ Direct/Tables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direct
+{
+    public class Tables
+    {
+        public void Multiples(int b)
+        {
+            for(int i = 1;i<=10;i++)
+            {
+                Console.WriteLine($"{b} * {i} = {b}");
+            }
+        }
+    }
+}
